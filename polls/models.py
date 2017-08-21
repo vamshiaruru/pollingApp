e3e050f9502b281abcf7cfcb3eb00cfe253376ea
vamshiaruru <vamshiaruru0@gmail.com>
2017-08-21 11:25:03 +0530

    Added first database connection. uses sqlite
    To make a new database, we have to modify models.py. Each class in
    models.py represents a table, and we can specify required settings
    in there.
    Then add polls.apps.PollsConfig (PollsConfig class is present in polls.
    apps) to installed_apps in settings.py, and run
    python manage.py makemigrations polls to let django know what we have
    done. This creates migrations, which is how django keeps track of our
    relational schema.
    To get what sql django is running, use python manage.py sqlmigrate
    polls 0001

--- polls/models.py
+++ polls/models.py
@@ -3,4 +3,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Questions(models.Model):
+    """
+    Each class here represents a table, and we define the various fields.
+    """
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("Publication Date")
+
+
+class Choice(models.Model):
+    """
+    Class to represent choice table. Has three fields, question, choice_text, and votes
+    question acts as foreign key pointing to Questions table
+    """
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
