3ed1d4e4f53af53810b2e827f43446ee4d864040
vamshiaruru <vamshiaruru0@gmail.com>
2017-08-21 13:19:56 +0530

    added a home page

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Questions, Choice
 
-# Register your models here.
+admin.site.register(Questions)
+admin.site.register(Choice)
--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def home(request):
+    return HttpResponse("Welcome to your Django project")
