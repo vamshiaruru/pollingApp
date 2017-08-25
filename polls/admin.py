67e9a76760598b110f83a45fa6adf241d522056b
vamshiaruru <vamshiaruru0@gmail.com>
2017-08-25 15:36:44 +0530

    made changes to django admin site

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,26 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Questions, Choice
 
-admin.site.register(Questions)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_recent')
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+    # The order is defined by the order in which we put the fieldsets. In this
+    #  case field containing question text appears first. The first element in
+    #  each tuple is the title of field set. We Can use the following instead
+    # too.
+    # fields = ['pub_date', 'question_text']
+    # The order in the fields array is the same order with which these fields
+    # appear in the admin site.
+    # or we can create field sets
+    inlines = [ChoiceInline]
+
+admin.site.register(Questions, QuestionAdmin)
